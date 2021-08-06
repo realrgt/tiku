3d99d2d8c76eabb0638b102ac7373dd0ee5a0ddf
realrgt <ergitorubildo@gmail.com>
2021-08-06 12:00:10 +0200

    countries datasources contracts

---
+++ lib/app/modules/country/data/datasources/country_remote_datasource.dart
@@ -0,0 +1,7 @@
+import '../../domain/entities/country.dart';
+
+abstract class ICountryRemoteDatasource {
+  Future<List<Country>> getAllCountries();
+  Future<List<Country>> filterCountriesByRegion(String region);
+  Future<List<Country>> searchCountriesByName(String countryName);
+}
