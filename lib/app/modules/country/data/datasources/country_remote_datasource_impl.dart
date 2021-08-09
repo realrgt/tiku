aa7be6e8921c7e61ea9b94da123a0643dc978869
realrgt <ergitorubildo@gmail.com>
2021-08-09 20:00:16 +0200

    country remote datasource impl -- get all countries

---
+++ lib/app/modules/country/data/datasources/country_remote_datasource_impl.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+import '../../../../core/errors/exceptions.dart';
+import '../models/country_model.dart';
+import 'country_remote_datasource.dart';
+
+class CountryRemoteDatasourceImpl implements ICountryRemoteDatasource {
+  final Dio client;
+
+  CountryRemoteDatasourceImpl(this.client);
+
+  @override
+  Future<List<CountryModel>> getAllCountries() async {
+    final response = await client.get('https://restcountries.eu/rest/v2/all');
+
+    if (response.statusCode != 200) throw ServerException();
+
+    List data = json.decode(response.data);
+    List<CountryModel> countries =
+        data.map((e) => CountryModel.fromJson(e)).toList();
+    return countries;
+  }
+
+  @override
+  Future<List<CountryModel>> filterCountriesByRegion(String region) async {
+    // TODO: implement filterCountriesByRegion
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<CountryModel>> searchCountriesByName(String keyword) async {
+    // TODO: implement searchCountriesByName
+    throw UnimplementedError();
+  }
+}
---
+++ test/app/modules/country/data/datasources/country_remote_datasource_impl_test.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:a_tiku/app/core/errors/exceptions.dart';
+import 'package:a_tiku/app/modules/country/data/datasources/country_remote_datasource_impl.dart';
+import 'package:a_tiku/app/modules/country/data/models/country_model.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../fixtures/fixtures_reader.dart';
+
+class MockDioClient extends Mock implements Dio {}
+
+void main() {
+  late CountryRemoteDatasourceImpl datasource;
+  late MockDioClient mockDioClient;
+
+  setUp(() {
+    mockDioClient = MockDioClient();
+    datasource = CountryRemoteDatasourceImpl(mockDioClient);
+  });
+
+  group('getAllCountries', () {
+    test(
+      'should return a list of Country when response code is 200 (success)',
+      () async {
+        // arrange
+        when(() => mockDioClient.get(any())).thenAnswer(
+          (_) async => Response(
+            requestOptions: RequestOptions(
+              path: 'https://restcountries.eu/rest/v2/all',
+            ),
+            data: fixture('countries.json'),
+            statusCode: 200,
+          ),
+        );
+        // act
+        final result = await datasource.getAllCountries();
+        // assert final tCountries
+        final expected = jsonDecode(fixture('countries.json'))
+            .map((e) => CountryModel.fromJson(e))
+            .toList();
+
+        verify(() => mockDioClient.get('https://restcountries.eu/rest/v2/all'));
+        expect(result, equals(expected));
+      },
+    );
+
+    test(
+      'should throw a ServerException when the response code aint 200 (failure)',
+      () async {
+        // arrange
+        when(() => mockDioClient.get(any())).thenAnswer(
+          (_) async => Response(
+            requestOptions: RequestOptions(
+              path: 'https://restcountries.eu/rest/v2/all',
+            ),
+            data: 'Something went wrong',
+            statusCode: 400,
+          ),
+        );
+        // act
+        final call = datasource.getAllCountries;
+        // assert final tCountries
+        expect(() => call(), throwsA(isA<ServerException>()));
+      },
+    );
+  });
+}
